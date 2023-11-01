c88aa437ed130dbe2e9e0b62609ce12f9184acb2
AbdulMoeidKhan <hamkhan088@gmail.com>
2023-11-01 21:04:04 +0500

    liasons

--- database/migrations/2023_11_01_092456_create_liasons_table.php
+++ database/migrations/2023_11_01_092456_create_liasons_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('liasons', function (Blueprint $table) {
             $table->id();
+            $table->uuid('liason_uid')->unique();
+            $table->string('liason_rank');
+            $table->string('liason_designation');
+            $table->string('liason_first_name');
+            $table->string('liason_last_name');
+            $table->integer('liason_contact');
+            $table->integer('liason_identity');
+            $table->uuid('liason_delegation')->unique()->nullable();
+            $table->uuid('liason_officer')->unique()->nullable();
+            $table->string('liasonCode')->unique();
+            $table->string('liason_assign')->default(0);
             $table->timestamps();
         });
     }
