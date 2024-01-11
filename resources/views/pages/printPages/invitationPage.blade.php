bc43779d1a1b9abcbb411e75c03828a583bc2b0a
unknown <you@example.com>
2024-01-11 16:03:46 +0500

    changes made for print

--- resources/views/pages/printPages/invitationPage.blade.php
+++ resources/views/pages/printPages/invitationPage.blade.php
@@ -14,7 +14,7 @@
 <div class="container-fluid">
     <div class="row btn-contain container">
         <div class="d-flex justify-content-center">
-            <button class="btn btn-outline-primary" onclick="window.print()">Print this E-badge</button>
+            <button class="btn btn-outline-primary" onclick="window.print()">Print this Invitation</button>
         </div>
         <br />
         <br />
@@ -86,37 +86,27 @@
     <div class="row container-first-child">
         <div class="col-md-12 parent-print-program">
             <div>
-                <!-- <img src="{{asset('images/Pimec.png')}}" class="logo-img" alt="Pimec" />
-                    <br /> -->
-
-                <p class="blockquote">
-                    Your E-Badge Information
-                </p>
-                <p>
-                </p>
-                <p>
-                    Registration Type : Trade Vistor
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla repellat eos nobis doloribus unde doloremque vel laborum id, distinctio mollitia dicta molestias consequuntur debitis vero obcaecati veniam omnis placeat laboriosam!
-                </p>
+            <br />
+                <h2>How to Print your e-Badge?</h2>
+              
+                <p>Go to the Badge Tab in Navigation Bar (Side Bar)</p>
+                    <p>
+                        Click on the Blue Button in a row to print required Badge
+                    </p>
             </div>
         </div>
 
     </div>
     <div class="row">
         <div class="col-md-12">
+        <br />
             <div>
-                <h2>Terms & conditions</h2>
+                <h2>Important Note:</h2>
                 <br />
                 <p class="blockquote">
                 <ol>
-                    <li class="list-group-item">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa animi, sunt nostrum omnis voluptatum, quidem tempore laborum saepe fugit aliquid ullam expedita molestias quis distinctio eligendi tenetur, qui sequi officiis!</li>
-                    <li class="list-group-item">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa animi, sunt nostrum omnis voluptatum, quidem tempore laborum saepe fugit aliquid ullam expedita molestias quis distinctio eligendi tenetur, qui sequi officiis!</li>
-                    <li class="list-group-item">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa animi, sunt nostrum omnis voluptatum, quidem tempore laborum saepe fugit aliquid ullam expedita molestias quis distinctio eligendi tenetur, qui sequi officiis!</li>
-                    <li class="list-group-item">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa animi, sunt nostrum omnis voluptatum, quidem tempore laborum saepe fugit aliquid ullam expedita molestias quis distinctio eligendi tenetur, qui sequi officiis!</li>
-                    <li class="list-group-item">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa animi, sunt nostrum omnis voluptatum, quidem tempore laborum saepe fugit aliquid ullam expedita molestias quis distinctio eligendi tenetur, qui sequi officiis!</li>
-                    <li class="list-group-item">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa animi, sunt nostrum omnis voluptatum, quidem tempore laborum saepe fugit aliquid ullam expedita molestias quis distinctio eligendi tenetur, qui sequi officiis!</li>
+                    <li class="list-group-item">If you have more than one delegate, you can enter that information with “Add New” and enter your required information and save.</li>
+                    <li class="list-group-item">For your participation confirmation on IDEAS 2022, you will need your e- Badge paper print along with further required documents at your “Arrival” in Pakistan. Please bring your e-Badge print copy and show our Airport Welcome Desk or assigned receiving officer.</li>
                 </ol>
                 </p>
             </div>
